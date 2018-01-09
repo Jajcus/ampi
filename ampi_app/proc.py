9bf0a0208580ebe518c7320afb460195fabbc108
Jacek Konieczny <j.konieczny@eggsoft.pl>
2018-01-09 15:48:33 +0100

    app: proc.Nanny: allow stdin/stdout communication

--- ampi_app/proc.py
+++ ampi_app/proc.py
@@ -95,44 +95,72 @@ def unblock_fd(stream):
 
 class Nanny:
     """Child process monitor."""
-    def __init__(self, name, command, kill_list=None, restart=True, callback=None):
+    def __init__(self, name, command, kill_list=None,
+                 restart=True, callback=None, stdout_callback=None,
+                 input_pipe=False):
         self.name = name
         self.command = command
         self.kill_list = kill_list
         self.logger = logging.getLogger("proc." + name)
         self.callback = callback
+        self.stdout_callback = stdout_callback
+        self.input_pipe = input_pipe
+        self._lock = threading.RLock()
         self._procs = Processes()
         self._child = None
-        self._thread = None
+        self._stdout_thread = None
+        self._stderr_thread = None
         self._should_be_running = False
         if kill_list:
             for name in kill_list:
                 self._procs.killall(name)
+
     def __del__(self):
         self.stop()
 
     def start(self):
-        if self._thread or self._child:
-            return
-        if self.kill_list:
-            for name in self.kill_list:
-                self._procs.killall(name)
+        with self._lock:
+            if self._stdout_thread or self._stderr_thread or self._child:
+                return
+            if self.kill_list:
+                for name in self.kill_list:
+                    self._procs.killall(name)
 
-        logger.info("Starting: %s", " ".join(self.command))
-        try:
-            self._child = subprocess.Popen(self.command,
-                                           stdin=open("/dev/null", "rb"),
-                                           stdout=subprocess.PIPE,
-                                           stderr=subprocess.STDOUT)
-        except OSError as err:
-            logger.error("Could not start %r: %s", " ".join(self.command), err)
-            return
-        self._thread = threading.Thread(target=self.process_output,
-                                        args=(self._child,),
-                                        name="{} nanny".format(self.name),
-                                        daemon=True)
-        self._should_be_running = True
-        self._thread.start()
+            logger.info("Starting: %s", " ".join(self.command))
+            try:
+                if self.input_pipe:
+                    stdin = subprocess.PIPE
+                else:
+                    stdin = open("/dev/null", "rb")
+                self._child = subprocess.Popen(self.command,
+                                               stdin=stdin,
+                                               stdout=subprocess.PIPE,
+                                               stderr=subprocess.PIPE)
+            except OSError as err:
+                logger.error("Could not start %r: %s", " ".join(self.command), err)
+                return
+
+            thread_args = [self._child, self._child.stdout]
+            if self.stdout_callback:
+                thread_args += [self.stdout_callback]
+            else:
+                thread_args += [self._log_output, logging.INFO]
+            thread_name = "{} nanny (stdout)".format(self.name)
+            self._stdout_thread = threading.Thread(target=self._output_thread,
+                                                   args=thread_args,
+                                                   name=thread_name,
+                                                   daemon=True)
+            self._stdout_thread.start()
+
+            thread_args = [self._child, self._child.stderr,
+                    self._log_output, logging.WARNING]
+            thread_name = "{} nanny (stderr)".format(self.name)
+            self._stderr_thread = threading.Thread(target=self._output_thread,
+                                                   args=thread_args,
+                                                   name=thread_name,
+                                                   daemon=True)
+            self._stderr_thread.start()
+            self._should_be_running = True
         if self.callback:
             self.callback(True)
 
@@ -140,46 +168,102 @@ class Nanny:
         self._should_be_running = False
 
     def stop(self):
-        self._should_be_running = False
-        child = self._child
-        if child:
-            logger.info("Terminating %s with SIGTERM...", self.name)
-            child.terminate()
-            try:
-                rc = child.wait(2)
-            except subprocess.TimeoutExpired:
-                logger.info("Killing %s with SIGKILL...", self.name)
-                child.kill()
-                time.sleep(0.2)
-            self._child = None
+        with self._lock:
+            self._should_be_running = False
+            child = self._child
+            if child:
+                if self.input_pipe and self._child.stdin:
+                    try:
+                        self._child.stdin.close()
+                    except OSError as err:
+                        logger.warning("%s stdin.close(): %s", self.name, err)
+                logger.info("Terminating %s with SIGTERM...", self.name)
+                child.terminate()
+                try:
+                    rc = child.wait(2)
+                except subprocess.TimeoutExpired:
+                    logger.info("Killing %s with SIGKILL...", self.name)
+                    child.kill()
+                    time.sleep(0.2)
+                self._child = None
 
-        if self.kill_list:
-            for name in self.kill_list:
-                self._procs.killall(name)
+            if self.kill_list:
+                for name in self.kill_list:
+                    self._procs.killall(name)
 
-        thread = self._thread
-        if thread:
+            threads = (self._stdout_thread, self._stderr_thread)
+
+        for thread in threads:
+            if not thread:
+                continue
             thread.join(5)
             if thread.is_alive():
-                logger.warning("{} nanny thread won't die".format(self.name))
-            self._thread = None
+                logger.warning("%s nanny thread %r won't die", self.name, thread)
 
-    def process_output(self, child):
-        """Proccess output of the child process."""
-        for line in child.stdout:
-            line = line.rstrip(b"\r\n").decode("utf-8", "replace")
-            self.logger.info("[%s] %s", self.name, line)
-        rc = child.wait()
-        self._thread = None
-        self._child = None
-        if rc > 0 or self._should_be_running:
-            self.logger.warning("%s exitted with status %i", self.name, rc)
+        with self._lock:
+            self._stdout_thread = None
+            self._stderr_thread = None
+
+    def write(self, data):
+        with self._lock:
+            if not self._child:
+                return
+            pipe = self._child.stdin
+        if isinstance(data, str):
+            data = data.encode("utf-8")
+        logger.debug("%s writing: %r", self.name, data)
+        pipe.write(data)
+        pipe.flush()
+
+    def _log_output(self, data, log_level):
+        local = threading.local()
+        if not hasattr(local, "buf"):
+            local.nanny_io_buf = b""
+            buf = data
         else:
-            self.logger.debug("%s exitted with status %i", self.name, rc)
-        if self.callback:
-            self.callback(False)
-        if self._should_be_running:
-            GObject.timeout_add(1000, self.restart_if_needed)
+            buf = local.nanny_io_buf + data
+
+        lines = buf.split(b"\n")
+        local.nanny_io_buf = lines[-1]
+
+        for line in lines[:-1]:
+            line = line.rstrip(b"\r").decode("utf-8", "replace")
+            self.logger.log(log_level, "[%s] %s", self.name, line)
+
+    def _output_thread(self, child, stream, callback, *args):
+        """Proccess output of the child process."""
+        while True:
+            try:
+                data = stream.read1(4096)
+            except OSError as err:
+                logger.warning("%s read(): %s", self.name, err)
+                break
+            if not data:
+                break
+            callback(data, *args)
+
+        rc = child.wait()
+
+        with self._lock:
+            thread = threading.current_thread()
+            if thread is self._stdout_thread:
+                self._stdout_thread = None
+            elif thread is self._stderr_thread:
+                self._stderr_thread = None
+
+            if not self._child:
+                # already handled by other thread
+                return
+
+            self._child = None
+            if rc > 0 or self._should_be_running:
+                self.logger.warning("%s exitted with status %i", self.name, rc)
+            else:
+                self.logger.debug("%s exitted with status %i", self.name, rc)
+            if self.callback:
+                self.callback(False)
+            if self._should_be_running:
+                GObject.timeout_add(1000, self.restart_if_needed)
 
     def restart_if_needed(self):
         if self._should_be_running:
@@ -199,7 +283,7 @@ class Nanny:
         self.start()
 
     def is_started(self):
-        return bool(self._thread) or bool(self._child)
+        return any((self._stdout_thread, self._stderr_thread, self._child))
 
 if __name__ == "__main__":
     print("Processes:")
